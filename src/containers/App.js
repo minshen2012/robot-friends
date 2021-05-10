ed4800aac69359dd4084ef429756bc2612b72887
mason <kuangminshen@gmail.com>
2021-05-10 16:53:41 +1000

    components and containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
